170bf9b08dc9b3de8a96100c24d5bd4379bfc7a1
Anup Dhondi <anupdhondi5@gmail.com>
2023-07-12 19:51:28 +0530

    learned query states, react-query devtools, query keys

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
+import { useState } from 'react';
 import './App.css';
 
 import { useQuery } from 'react-query';
 
-function App() {
+//1. If the component is unmounted then the query will be in inactive state
+//2. As the component mounts it fetches the query and goes into state
+//3. Even if the app window/ browser tab is out focused and then in focused then als it will fetch and goes to stale
+
+function Button() {
   const { isLoading, isError, data, error } = useQuery('unique-key', () => {
     return new Promise((res, rej) => {
-      rej(5);
+      setTimeout(() => res(Math.random()), 1000);
     });
   });
-  console.log(
-    '🚀 ~ file: App.js:11 ~ const{data,error}=useQuery ~ data:',
-    data,
-    error
-  );
+  return <button>Submit {data}</button>;
+}
 
-  return <div className="App">asdsad</div>;
+function App() {
+  const [show, setShow] = useState(true);
+  return (
+    <div className="App">
+      {show && <Button />}
+      <br />
+      <button onClick={() => setShow(!show)}>toggle</button>
+    </div>
+  );
 }
 
 export default App;
