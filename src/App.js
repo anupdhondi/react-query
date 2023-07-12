cb9c6a6b371079faf21d69c8168c0398aae33559
Anup Dhondi <anupdhondi5@gmail.com>
2023-07-12 18:33:45 +0530

    Intro for react query done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
+import { useQuery } from 'react-query';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const { isLoading, isError, data, error } = useQuery('unique-key', () => {
+    return new Promise((res, rej) => {
+      rej(5);
+    });
+  });
+  console.log(
+    '🚀 ~ file: App.js:11 ~ const{data,error}=useQuery ~ data:',
+    data,
+    error
   );
+
+  return <div className="App">asdsad</div>;
 }
 
 export default App;
