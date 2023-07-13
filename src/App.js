b35f72ee7b1c73cef90bf0ed979503efb7a7a6bb
Anup Dhondi <anupdhondi5@gmail.com>
2023-07-13 20:23:26 +0530

    dependent queries and enabled learned about it

--- src/App.js
+++ src/App.js
@@ -3,24 +3,27 @@ import './App.css';
 
 import { useQuery } from 'react-query';
 
-//1. If the component is unmounted then the query will be in inactive state
-//2. As the component mounts it fetches the query and goes into state
-//3. Even if the app window/ browser tab is out focused and then in focused then als it will fetch and goes to stale
-
-function Button() {
-  const { isLoading, isError, data, error } = useQuery('unique-key', () => {
-    return new Promise((res, rej) => {
-      setTimeout(() => res(Math.random()), 1000);
-    });
-  });
-  return <button>Submit {data}</button>;
-}
-
 function App() {
-  const [show, setShow] = useState(true);
+  const [show, setShow] = useState(false);
+
+  const { isIdle } = useQuery(
+    ['unique-key', show],
+    () => {
+      return new Promise((res, rej) => {
+        setTimeout(() => res(Math.random()), 1000);
+      });
+    },
+    {
+      enabled: show,
+    }
+  );
+  console.log(
+    '🚀 ~ file: App.js:16 ~ const{isIdle}=useQuery ~ isIdle:',
+    isIdle
+  );
+
   return (
     <div className="App">
-      {show && <Button />}
       <br />
       <button onClick={() => setShow(!show)}>toggle</button>
     </div>
