82ee65c8211dfbb18c44f303ba8dbd45fa4372aa
Anup Dhondi <anupdhondi5@gmail.com>
2023-07-25 17:43:13 +0530

    Undestood basics of mutaions

--- src/App.js
+++ src/App.js
@@ -1,67 +1,32 @@
 import { useState } from 'react';
 import './App.css';
 
-import { useQuery } from 'react-query';
+import { useMutation, useQuery } from 'react-query';
 import Post from './Post';
 import client from './react-query-client';
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
-
+const timer = (duration) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log('In Promise');
+      resolve();
+    }, duration);
+  });
+};
 function App() {
-  const [postID, setPostID] = useState(null);
+  const mutation = useMutation(() => timer(1000));
 
-  const { isLoading, data } = useQuery(
-    ['posts'],
-    () => fetcher(`https://jsonplaceholder.typicode.com/posts`),
-    {
-      staleTime: Infinity,
-      cacheTime: 5000,
-    }
-  );
-
-  const clearPostID = () => setPostID(null);
-
-  if (postID) {
-    return <Post postID={postID} clearPostID={clearPostID} />;
-  }
-
-  if (isLoading) {
-    return (
-      <div className="App">
-        <h2>Loading...</h2>
-      </div>
-    );
-  }
-
-  function mutateTitle(id) {
-    client.setQueryData(['post', id], (oldData) => {
-      if (oldData) {
-        return {
-          ...oldData,
-          title: 'I mutated it manually',
-        };
-      }
-    });
-  }
+  const mutationClicked = async () => {
+    console.log('Callling Mutation');
+    await mutation.mutateAsync();
+    console.log('Done...');
+  };
 
   return (
     <div className="App">
-      <div>
-        {/* <input value={repoName} onChange={(e) => setRepoName(e.target.value)} /> */}
-        {data.map((el) => {
-          const cachedPost = client.getQueryData(['post', el.id]);
-
-          return (
-            <p key={el.id}>
-              {cachedPost ? '(Visited)' : ''}
-              <a onClick={() => setPostID(el.id)} href="#">
-                {el.id} - {el.title}
-              </a>
-              <button onClick={() => mutateTitle(el.id)}>mutate title</button>
-            </p>
-          );
-        })}
-      </div>
+      <p>Mutations</p>
+      <button onClick={mutationClicked}>Submit</button>
     </div>
   );
 }
