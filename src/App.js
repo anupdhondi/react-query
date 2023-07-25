3d91f2de6cce0ae9016b992cccfdbbe2d511da04
Anup Dhondi <anupdhondi5@gmail.com>
2023-07-25 17:48:48 +0530

    Sideeffects of mutations

--- src/App.js
+++ src/App.js
@@ -7,19 +7,38 @@ import client from './react-query-client';
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
 const timer = (duration) => {
-  return new Promise((resolve) => {
+  return new Promise((resolve, reject) => {
     setTimeout(() => {
-      console.log('In Promise');
-      resolve();
+      if (Math.random() > 0.5) {
+        resolve('succeeded');
+      } else {
+        reject('Rejected / Failed');
+      }
     }, duration);
   });
 };
+
 function App() {
-  const mutation = useMutation(() => timer(1000));
+  const mutation = useMutation(() => timer(1000), {
+    onSuccess(data) {
+      console.log('🚀 ~ file: App.js:20 ~ onSuccess ~ data:', data);
+    },
+    onError(data) {
+      console.log('🚀 ~ file: App.js:26 ~ onError ~ data:', data);
+    },
+    onSettled(data, error) {
+      console.log('🚀 ~ file: App.js:29 ~ onSettled ~ error:', error);
+      console.log('🚀 ~ file: App.js:29 ~ onSettled ~ data:', data);
+    },
+  });
 
   const mutationClicked = async () => {
     console.log('Callling Mutation');
-    await mutation.mutateAsync();
+    try {
+      await mutation.mutateAsync();
+    } catch (error) {
+      console.log('🚀 ~ file: App.js:39 ~ mutationClicked ~ error:', error);
+    }
     console.log('Done...');
   };
 
